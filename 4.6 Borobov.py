09f4dc0c35d8493cd384779f5b612d0946d6dd92
Dmitry Borobov <gclassic.one@gmail.com>
2023-02-09 22:47:10 +0700

    4.6 Theme

---
+++ 4.6 Borobov.py
@@ -0,0 +1,33 @@
+# Задачи 4.6 Junior-
+from random import randint
+"""
+#1 Task
+a = randint(0, 100)
+b = randint(0, 100)
+c = randint(0, 100)
+print(f'Число а = {a}, число b = {b}, число c = {c}, среднее = ', round((a+b+c) / 3, 2))
+"""
+"""
+#2 Task
+x = randint(0, 100)
+y = randint(0, 100)
+print(f'Число x = {x}, число y = {y}, DIV = {x // y}, MOD = {x % y}')
+"""
+"""
+#3 Task
+x = 14.721
+print(f'1) {round(x,2)}')
+print(f'2) {round(x)}')
+print('{0:=011}'.format(x))
+"""
+
+"""
+#Задача Junior
+string_number = input('Enter your number = ')
+number = int(string_number)
+string_number = string_number[::-1]
+if number < 0:
+    print(f'-{string_number[:len(string_number) - 1]}')
+else:
+    print(string_number)
+"""
